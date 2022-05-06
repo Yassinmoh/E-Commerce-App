27f1a470af2684501d321f8c9368e57d2bbfaf3e
yassin mohamed <yassin_mohamed310@outlook.com>
2022-05-06 02:17:14 +0200

    Create Layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,30 @@
 import React from 'react'
+import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
 const Navbar = () => {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <div className="container">
-                <div className="logo"> LOGO</div>
-                <ul>
-                    <li>
-                        <Link href='/'>
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/about'>
-                            <a>About</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/cart'>
-                            <a>Cart</a>
-                        </Link>
-                    </li>
-                </ul>
+                <div className={styles.container}>
+                    <div className={styles.logo}> LOGO</div>
+                    <ul>
+                        <li>
+                            <Link href='/'>
+                                <a>Home</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='/about'>
+                                <a>About</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='/cart'>
+                                <a>Cart</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
